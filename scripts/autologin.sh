110b379e709731c3f11e11fa2f0b21ad0f744c47
abist_kobayashi <hi_kobayashi@abist.co.jp>
2021-03-27 19:40:58 +0000

    envファイルの修正

---
+++ scripts/autologin.sh
@@ -0,0 +1,19 @@
+#!bin/bash 
+expect -c "
+set timeout 5
+spawn oj login https://atcoder.jp
+expect \"Username: \"
+send \"$ATCODER_USERNAME\n\"
+expect \"Password: \"
+send \"$ATCODER_PASSWORD\n\"
+
+spawn acc login
+set timeout 5
+expect \"? username: \"
+send \"$ATCODER_USERNAME\n\"
+set timeout 1
+expect \"? password: \"
+send \"$ATCODER_PASSWORD\n\"
+interact
+"
+    
