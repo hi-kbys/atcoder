ceb0ce835e7786718f6ac4d4918aa2b4d18fa130
h-kobayashi <81208066+abist-kobayashi@users.noreply.github.com>
2022-12-24 07:20:12 +0000

    atcoder-toolsへの移行

--- scripts/autologin.py
+++ scripts/autologin.py
@@ -4,23 +4,18 @@ from subprocess import PIPE
 from shlex import quote
 import time
 
-atcoder_username = os.environ["ATCODER_USERNAME"]
-atcoder_password = os.environ["ATCODER_PASSWORD"]
+def main():
+    atcoder_username = os.environ["ATCODER_USERNAME"]
+    atcoder_password = os.environ["ATCODER_PASSWORD"]
 
-command = "oj login https://atcoder.jp"
-with subprocess.Popen(command,shell=True,stdin=PIPE,stdout=PIPE,stderr=PIPE,text=True) \
-    as proc:
-    while proc.poll() is None:
-        o = proc.stdout.readline()
-        print(o)
-        if o == '[NETWORK] 200 OK\n':
-            break
-    inputs = atcoder_username+'\n'+atcoder_password+'\n'
-    try:
-        stdout, stderr = proc.communicate(inputs,1)
-    except :
-        print(stdout)
-        print(stderr)
-    proc.communicate(inputs,1)
+    url = "https://atcoder.jp"
+    command = [
+        "oj", "login",
+        "-u", atcoder_username, "-p", atcoder_password, 
+        "--check", url
+        ]
+
+    subprocess.run(command, capture_output = True, check=True)
     
-print('owari')
+if __name__ == "__main__":
+    main()
